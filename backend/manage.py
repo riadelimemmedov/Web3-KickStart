fc6840222e077fe57f8d7dfb61574bca1ba9c3ef
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-02-25 21:20:35 +0400

    Add Docker

--- backend/manage.py
+++ backend/manage.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify,render_template, request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from flask_cors import CORS
+from decouple import config
 
 
 #!Python Modules
@@ -13,10 +14,13 @@ from datetime import datetime
 #*configution
 DEBUG=True
 
+print('Database Url Value ', config("DATABASE_URL"))
+
 
 #*instantiate the app
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = config("DATABASE_URL")
+db = SQLAlchemy(app)
 
 
 #*enable CORS
@@ -28,7 +32,7 @@ cors = CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route('/',methods=['GET'])
 def pingPongView():
-    return {'Ping':'Pong'}
+    return {'Ping':'HE'}
 
 
 if __name__ =='__main__':  
